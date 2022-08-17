832877a7522f0eef1eae731394e4a908f3113788
Johan Melin <soldierheart@hotmail.com>
2022-08-17 16:51:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//ES6 module
+import camelCase from 'lodash/camelCase' //531.35kb, 68.5kb, 24.06kb
+//const camelCase = require('lodash/camelCase') //1.49kb. 773b, 482b
+//const {camelCase} = require('lodash') //531.35kb
+//const lodash = require('lodash') //531.35kb
+
+console.log(lodash.camelCase('WTF'))
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+            },
+        ]
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 9000
+    }
+}
